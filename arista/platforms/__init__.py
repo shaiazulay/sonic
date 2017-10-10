283e2c93bc931a363250d34c9eec6463319d4c90
Zhi Yuan Carl Zhao <zyzhao@arista.com>
2017-10-10 10:04:48 -0700

    Add basic platform support for DCS-7170-64
    Change-Id: I87669a82180a630fdb8c6f3315ffda957838f831

--- arista/platforms/__init__.py
+++ arista/platforms/__init__.py
@@ -2,4 +2,5 @@
 from . import a7050qx32
 from . import a7050qx32s
 from . import a7060cx32s
+from . import a717064c
 from . import a7260cx364
--- arista/platforms/a717064c.py
+++ arista/platforms/a717064c.py
@@ -4,13 +4,13 @@ from ..core.utils import incrange
 from ..core.types import PciAddr, I2cAddr, NamedGpio, ResetGpio
 from ..core.component import Priority
 
-from ..components.common import SwitchChip, I2cKernelComponent
+from ..components.common import I2cKernelComponent
 from ..components.scd import Scd
 
-@registerPlatform('DCS-7260CX3-64')
-class Gardena(Platform):
+@registerPlatform('DCS-7170-64')
+class Alhambra(Platform):
    def __init__(self):
-      super(Gardena, self).__init__()
+      super(Alhambra, self).__init__()
 
       self.sfpRange = incrange(65, 66)
       self.qsfpRange = incrange(1, 64)
@@ -20,39 +20,28 @@ class Gardena(Platform):
       self.addDriver(KernelDriver, 'rook-fan-cpld')
       self.addDriver(KernelDriver, 'rook-led-driver')
 
-      switchChip = SwitchChip(PciAddr(bus=0x07))
-      self.addComponent(switchChip)
-
-      scd = Scd(PciAddr(bus=0x06))
+      scd = Scd(PciAddr(bus=0x06), newDriver=True)
       self.addComponent(scd)
 
-      self.inventory.addWatchdog(scd.createWatchdog())
-
       scd.addComponents([
-         I2cKernelComponent(I2cAddr(1, 0x4c), 'max6658', '/sys/class/hwmon/hwmon1'),
-         I2cKernelComponent(I2cAddr(3, 0x58), 'pmbus',
+         I2cKernelComponent(I2cAddr(8, 0x4c), 'max6658'),
+         I2cKernelComponent(I2cAddr(6, 0x58), 'dps1900',
                             priority=Priority.BACKGROUND),
-         I2cKernelComponent(I2cAddr(4, 0x58), 'pmbus',
+         I2cKernelComponent(I2cAddr(7, 0x58), 'dps1900',
                             priority=Priority.BACKGROUND),
-      ]) # Incomplete
+      ])
 
       scd.addSmbusMasterRange(0x8000, 8, 0x80)
 
       scd.addResets([
-         ResetGpio(0x4000, 0, False, 'switch_chip_reset'),
-         ResetGpio(0x4000, 1, False, 'switch_chip_pcie_reset'),
-         ResetGpio(0x4000, 2, False, 'security_asic_reset'),
+         ResetGpio(0x4000, 8, False, 'switch_chip_reset'),
+         ResetGpio(0x4000, 1, False, 'security_chip_reset'),
       ])
 
       scd.addGpios([
          NamedGpio(0x5000, 0, True, False, "psu1_present"),
          NamedGpio(0x5000, 1, True, False, "psu2_present"),
-         NamedGpio(0x5000, 8, True, False, "psu1_status"),
-         NamedGpio(0x5000, 9, True, False, "psu2_status"),
-         NamedGpio(0x5000, 10, True, False, "psu1_ac_status"),
-         NamedGpio(0x5000, 11, True, False, "psu2_ac_status"),
       ])
-      self.inventory.addPsus([scd.createPsu(1, True), scd.createPsu(2, True)])
 
       addr = 0x6100
       for xcvrId in self.qsfpRange:
@@ -62,7 +51,7 @@ class Gardena(Platform):
             self.inventory.addXcvrLed(xcvrId, name)
             addr += 0x10
 
-      addr = 0x7100
+      addr = 0x7200
       for xcvrId in self.sfpRange:
          name = "sfp%d" % xcvrId
          scd.addLed(addr, name)
@@ -70,24 +59,22 @@ class Gardena(Platform):
          addr += 0x10
 
       addr = 0xA010
-      bus = 9
+      bus = 7
       for xcvrId in sorted(self.qsfpRange):
          xcvr = scd.addQsfp(addr, xcvrId, bus)
          self.inventory.addXcvr(xcvr)
          scd.addComponent(I2cKernelComponent(
             I2cAddr(bus, xcvr.eepromAddr), 'sff8436'))
-         scd.addBusTweak(bus, xcvr.eepromAddr)
          addr += 0x10
          bus += 1
 
-      addr = 0xA410
-      bus = 7
+      addr = 0xA500
+      bus = 9
       for xcvrId in sorted(self.sfpRange):
          xcvr = scd.addSfp(addr, xcvrId, bus)
          self.inventory.addXcvr(xcvr)
          scd.addComponent(I2cKernelComponent(
             I2cAddr(bus, xcvr.eepromAddr), 'sff8436'))
-         scd.addBusTweak(bus, xcvr.eepromAddr)
          addr += 0x10
          bus += 1
 
@@ -96,11 +83,10 @@ class Gardena(Platform):
 
       cpld.addSmbusMasterRange(0x8000, 4, 0x80, 4)
       cpld.addComponents([
-         I2cKernelComponent(I2cAddr(73, 0x4c), 'max6658', '/sys/class/hwmon/hwmon2'),
-         # Handling of the DPM is disabled because this functionality is unstable.
-         #I2cKernelComponent(I2cAddr(74, 0x4e), 'pmbus',
-         #                   priority=Priority.BACKGROUND),
-         I2cKernelComponent(I2cAddr(85, 0x60), 'rook_cpld', '/sys/class/hwmon/hwmon3'),
+         I2cKernelComponent(I2cAddr(73, 0x4c), 'max6658'),
+         I2cKernelComponent(I2cAddr(74, 0x4e), 'pmbus',
+                            priority=Priority.BACKGROUND),
+         I2cKernelComponent(I2cAddr(85, 0x60), 'rook_cpld'),
          I2cKernelComponent(I2cAddr(88, 0x20), 'rook_leds'),
          I2cKernelComponent(I2cAddr(88, 0x48), 'lm73'),
       ])
