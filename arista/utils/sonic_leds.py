5de648720f5be3f87b6a6f85ae21d5206419c44e
Jeff Chan <jchan@arista.com>
2017-12-18 14:46:49 -0800

    Add LedPlugin support for ceos.
    Change-Id: I345f8d97d0403afde2fafb34a12b16a95c2e73dd

--- arista/utils/sonic_leds.py
+++ arista/utils/sonic_leds.py
@@ -1,36 +1,33 @@
-from collections import defaultdict
-
-from arista.utils.sonic_utils import parsePortConfig
-from ..core import platform
-
 # DO NOT REMOVE THIS LINE. Needed to import all the platform info.
 import arista.platforms
+
+from collections import defaultdict
+
+from arista.utils.sonic_utils import parsePortConfig, getSonicVersVar
+from ..core import platform
 
 try:
    from sonic_led import led_control_base
 except ImportError as e:
    raise ImportError('%s - required module not found' % str(e))
 
-class LedControl(led_control_base.LedControlBase):
-   LED_SYSFS_PATH = "/sys/class/leds/{0}/brightness"
-
-   LED_COLOR_OFF = 0
-   LED_COLOR_GREEN = 1
-   LED_COLOR_YELLOW = 2
+class LedControlCommon(led_control_base.LedControlBase):
+   LED_COLOR_OFF = None
+   LED_COLOR_GREEN = None
+   LED_COLOR_YELLOW = None
 
    def __init__(self):
       self.portMapping = parsePortConfig()
-      self.portSysfsMapping = defaultdict(list)
-
-      inventory = platform.getPlatform().getInventory()
-      for port, names in inventory.xcvrLeds.items():
-         for name in names:
-            self.portSysfsMapping[port].append(self.LED_SYSFS_PATH.format(name))
-
+      self.inventory = platform.getPlatform().getInventory()
       # Set status leds to green initially (Rook led driver does this automatically)
-      for statusLed in inventory.statusLeds:
-         with open(self.LED_SYSFS_PATH.format(statusLed), 'w') as fp:
-            fp.write('%d' % self.LED_COLOR_GREEN)
+      for statusLed in self.inventory.statusLeds:
+         self._setStatusColor(statusLed, self.LED_COLOR_GREEN)
+
+   def _setStatusColor(self, invStatusLed, color):
+      raise NotImplementedError('Missing override of _setStatusColor')
+
+   def _setIntfColor(self, port, idx, color):
+      raise NotImplementedError('Missing override of _setIntfColor')
 
    def port_link_state_change(self, port, state):
       '''
@@ -41,17 +38,70 @@ class LedControl(led_control_base.LedControlBase):
       if not p:
          return
       for idx in range(p.lanes):
-         path = self.portSysfsMapping[p.portNum][p.offset + idx]
-         with open(path, 'w') as fp:
-            if state == 'up':
-               if idx == 0:
-                  fp.write('%d' % self.LED_COLOR_GREEN)
-               else:
-                  fp.write('%d' % self.LED_COLOR_YELLOW)
-            elif state == 'down':
-               fp.write('%d' % self.LED_COLOR_OFF)
+         if state == 'up':
+            if idx == 0:
+               self._setIntfColor(p, idx, self.LED_COLOR_GREEN)
+            else:
+               self._setIntfColor(p, idx, self.LED_COLOR_YELLOW)
+         elif state == 'down':
+            self._setIntfColor(p, idx, self.LED_COLOR_OFF)
          if p.singular:
             return
 
+class LedControlSysfs(LedControlCommon):
+   LED_SYSFS_PATH = "/sys/class/leds/{0}/brightness"
+
+   LED_COLOR_OFF = 0
+   LED_COLOR_GREEN = 1
+   LED_COLOR_YELLOW = 2
+
+   def __init__(self):
+      LedControlCommon.__init__(self)
+      self.portSysfsMapping = defaultdict(list)
+      for port, names in self.inventory.xcvrLeds.items():
+         for name in names:
+            self.portSysfsMapping[port].append(self.LED_SYSFS_PATH.format(name))
+
+   def _setStatusColor(self, invStatusLed, color):
+      with open(self.LED_SYSFS_PATH.format(invStatusLed), 'w') as fp:
+         fp.write('%d' % self.LED_COLOR_GREEN)
+
+   def _setIntfColor(self, port, idx, color):
+      path = self.portSysfsMapping[port.portNum][port.offset + idx]
+      with open(path, 'w') as fp:
+         fp.write('%d' % color)
+
+class LedControlCeos(LedControlCommon):
+   LED_COLOR_OFF = 'off'
+   LED_COLOR_GREEN = 'green'
+   LED_COLOR_YELLOW = 'yellow'
+
+   def __init__(self):
+      from jsonrpclib import Server
+      self.ceos = Server("http://localhost:8080/command-api)")
+      #TODO: add status and check fan_status equivalent in EOS
+      self.invToCliStatusLedMap = {'status' : 'status',
+                                   'fan_status' : 'fantray',
+                                   'psu(?P<num>\d+' : 'powersupply',
+                                   'beacon' : 'chassis'}
+      LedControlCommon.__init__(self)
+
+   def _setStatusColor(self, invStatusLed, color):
+      # TODO: map this to cEOS CLI calls.
+      pass
+
+   def _setIntfColor(self, port, idx, color):
+      ceosIntfName = "Ethernet%d" % port.portNum
+      if not port.singular:
+         ceosIntfName = "%s/%s" % (ceosIntfName, idx + 1)
+      try:
+         self.ceos.runCmds(1,
+                           ['enable', 'led interface %s %s' % (ceosIntfName, color)])
+      except:
+         pass
+      return
+
 def getLedControl():
-   return LedControl
+   if getSonicVersVar('asic_type') == 'ceos':
+      return LedControlCeos
+   return LedControlSysfs
