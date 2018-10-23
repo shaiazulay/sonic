7f222de07a46141a818b3d4804df0a2134ff2dac
Zhi Yuan Carl Zhao <zyzhao@arista.com>
2018-10-23 12:51:17 -0700

    Fix port_config.ini interpretation for 7170
    Change-Id: I0648e56db7b1d15226663584a8f24feb73073bcc

--- arista/utils/sonic_utils.py
+++ arista/utils/sonic_utils.py
@@ -5,46 +5,41 @@ from collections import namedtuple
 
 from ..core.utils import runningInContainer
 
-Port = namedtuple('Port', ['portNum', 'lanes', 'offset', 'singular'])
+Port = namedtuple('Port', ['portNum', 'lanes', 'singular', 'alias'])
 
 def parsePortConfig():
    '''
    Returns a dictionary mapping port name ("Ethernet48") to a named tuple of port
-   number, # of lanes, the offset (0 to 3 from the first lane in qsfp) and the
+   number, # of lanes, and the
    singularity of the lane (if it is in 100G/40G mode)
    '''
    portMapping = {}
 
    with open(portConfigPath()) as fp:
+      header = fp.readline()[1:].split()
+      headerMap = {key.strip(): idx for (idx, key) in enumerate(header)}
       for line in fp:
          line = line.strip()
          if not line or line[0] == '#':
             continue
 
          fields = line.split()
-         # "portNum" is determined from the fourth column (port), or the first number
-         # in the third column (alias).
-         # "lanes" is determined from the number of lanes in the second column.
-         # "offset" is determined from the second number in the third column (alias).
+         # "portNum" is determined from the "index" column or derived from the
+         # "alias" column.
+         # "lanes" is determined from the number of lanes in the "lanes" column.
          # "singular" is determined by if the alias has a '/' character or not.
-         if len(fields) < 3:
-            continue
-         name = fields[0]
-         lanes = len(fields[1].split(','))
-         alias = fields[2]
+         name = fields[headerMap['name']]
+         lanes = len(fields[headerMap['lanes']].split(','))
+         alias = fields[headerMap['alias']]
          aliasRe = re.findall(r'\d+', alias)
-         try:
-            portNum = int(fields[3])
-         except IndexError:
-            portNum = int(aliasRe[0])
+         portNum = int(fields[headerMap['index']]) if headerMap.get('index') else \
+                   int(aliasRe[0])
          if len(aliasRe) < 2:
-            offset = 0
             singular = True
          else:
-            offset = int(aliasRe[1]) - 1
             singular = False
 
-         portMapping[name] = Port(portNum, lanes, offset, singular)
+         portMapping[name] = Port(portNum, lanes, singular, alias)
 
    return portMapping
 
