13a3fc5de480d249c8f19cda15dfbb4a2af5bbb1
Zhi Yuan Carl Zhao <zyzhao@arista.com>
2019-07-08 14:49:08 -0700

    Add get_eeprom_dict method for sonic_eeprom
    Change-Id: I5030d9d1310fe26c4e6cdf36e69494bd989c0bbb

--- arista/utils/sonic_eeprom.py
+++ arista/utils/sonic_eeprom.py
@@ -10,28 +10,29 @@ The eeprom plugin end up being just the following
    import arista.utils.sonic_eeprom
    board = arista.utils.sonic_eeprom.getTlvInfoDecoder()
 
+We should be able to remove "getTlvInfoDecoder" after we change it on
+sonic-buildimage, and do:
+
+   from arista.utils.sonic_eeprom import board
 """
 
 from __future__ import absolute_import
 
 import StringIO
-import os
 
 from ..core import prefdl
 from ..core.platform import fmted_prefdl_path
 
 try:
    from sonic_eeprom import eeprom_base
-   from sonic_eeprom import eeprom_tlvinfo
-except ImportError as e:
-   raise ImportError (str(e) + " - required module not found")
+except ImportError as error:
+   raise ImportError("%s - required module not found" % error)
 
-class board(eeprom_tlvinfo.TlvInfoDecoder):
-
+class board(eeprom_base.EepromDecoder):
    def __init__(self, name, path, cpld_root, ro):
       self._prefdl_cache = {}
       self.prefdl_path = fmted_prefdl_path
-      super(board, self).__init__(self.prefdl_path, 0, '', True)
+      super(board, self).__init__(self.prefdl_path, None, 0, '', True)
 
    def read_eeprom(self):
       with open(self.prefdl_path) as fp:
@@ -48,20 +49,24 @@ class board(eeprom_tlvinfo.TlvInfoDecoder):
       return pfdl
 
    def decode_eeprom(self, e):
-       pfdl = self._decode_eeprom(e)
-       return pfdl.show()
+      pfdl = self._decode_eeprom(e)
+      return pfdl.show()
 
    def is_checksum_valid(self, e):
-       pfdl = self._decode_eeprom(e)
-       return (True, pfdl.getCrc())
+      pfdl = self._decode_eeprom(e)
+      return (True, pfdl.getCrc())
 
    def serial_number_str(self, e):
-       pfdl = self._decode_eeprom(e)
-       return pfdl.getField('SerialNumber')
+      pfdl = self._decode_eeprom(e)
+      return pfdl.getField('SerialNumber')
 
-   def mgmtaddrstr(self,e):
-       pfdl = self._decode_eeprom(e)
-       return pfdl.getField('MAC')
+   def mgmtaddrstr(self, e):
+      pfdl = self._decode_eeprom(e)
+      return pfdl.getField('MAC')
+
+   def get_eeprom_dict(self, e):
+      pfdl = self._decode_eeprom(e)
+      return {'Data': pfdl.data()}
 
 def getTlvInfoDecoder():
    return board
