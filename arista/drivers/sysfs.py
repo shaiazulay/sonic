dacc6d9275926c53ec5ba6554039171b729487b2
Zhi Yuan Carl Zhao <zyzhao@arista.com>
2019-09-06 09:50:11 -0700

    Move finding sysfs path in fan to function
    Change-Id: I4b05d7adc4ecd642ef4e4c713f6a6afd551abe71

--- arista/drivers/sysfs.py
+++ arista/drivers/sysfs.py
@@ -96,40 +96,35 @@ class FanSysfsDriver(SysfsDriver):
       super(FanSysfsDriver, self).setup()
       self.fileWaiter.waitFileReady()
 
-   # Fan speeds are a percentage
-   def getFanSpeed(self, fan):
+   def computeSysfsPath(self, fanId):
       if not self.sysfsPath:
          self.sysfsPath = utils.locateHwmonPath(
-               self.addr.getSysfsPath(), 'pwm%s' % fan.fanId)
+               self.addr.getSysfsPath(), 'pwm%s' % fanId)
+
+   # Fan speeds are a percentage
+   def getFanSpeed(self, fan):
+      self.computeSysfsPath(fan.fanId)
       return int(float(self.read('pwm%s' % fan.fanId)) / self.maxPwm * 100)
 
    def setFanSpeed(self, fan, speed):
+      self.computeSysfsPath(fan.fanId)
       if not int(speed) in range(101):
          logging.error('invalid speed setting %s for fan %s', speed, fan.fanId)
          return None
-      if not self.sysfsPath:
-         self.sysfsPath = utils.locateHwmonPath(
-               self.addr.getSysfsPath(), 'pwm%s' % fan.fanId)
       logging.debug('setting fan %s speed to %s', fan.fanId, speed)
       return self.write('pwm%s' % fan.fanId,
                         str(int(int(speed) * 0.01 * self.maxPwm)))
 
    def getFanDirection(self, fan):
-      if not self.sysfsPath:
-         self.sysfsPath = utils.locateHwmonPath(
-               self.addr.getSysfsPath(), 'pwm%s' % fan.fanId)
+      self.computeSysfsPath(fan.fanId)
       return self.read('fan%s_airflow' % fan.fanId)
 
    def getFanPresence(self, fan):
-      if not self.sysfsPath:
-         self.sysfsPath = utils.locateHwmonPath(
-               self.addr.getSysfsPath(), 'pwm%s' % fan.fanId)
+      self.computeSysfsPath(fan.fanId)
       return bool(int(self.read('fan%s_present' % fan.fanId)))
 
    def getFanStatus(self, fan):
-      if not self.sysfsPath:
-         self.sysfsPath = utils.locateHwmonPath(
-               self.addr.getSysfsPath(), 'pwm%s' % fan.fanId)
+      self.computeSysfsPath(fan.fanId)
       try:
          return not bool(int(self.read('fan%s_fault' % fan.fanId)))
       except IOError:
