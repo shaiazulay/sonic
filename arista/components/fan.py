f8abcb80e3e1ce65de8cd41d13ac02f3927d85ea
Zhi Yuan Carl Zhao <zyzhao@arista.com>
2019-09-09 15:52:07 -0700

    Add fans in platform declaration for 7280
    Change-Id: Ib5cff2c7e283508507d960273adbdccdbd195a7a

--- arista/components/fan.py
+++ arista/components/fan.py
@@ -8,7 +8,7 @@ from ..core.driver import KernelDriver
 from ..drivers.i2c import I2cKernelFanDriver
 from ..drivers.sysfs import FanSysfsDriver, LedSysfsDriver
 
-from .common import I2cComponent
+from .common import I2cComponent, PciComponent
 
 class CrowFanCpldComponent(I2cComponent):
    def __init__(self, addr=None, drivers=None, waitFile=None, **kwargs):
@@ -75,6 +75,22 @@ class RavenFanCpldComponent(I2cComponent):
    def createFan(self, fanId, driver='FanSysfsDriver', ledDriver='LedSysfsDriver',
                  **kwargs):
       logging.debug('creating raven fan %s', fanId)
+      driver = self.drivers[driver]
+      led = LedImpl(name='fan%s' % fanId, driver=self.drivers[ledDriver])
+      return FanImpl(fanId=fanId, driver=driver, led=led, **kwargs)
+
+class ScdFanComponent(PciComponent):
+   def __init__(self, drivers=None, waitFile=None, **kwargs):
+      fanSysfsDriver = FanSysfsDriver(maxPwm=255,
+            sysfsPath='/sys/devices/pci0000:00/0000:00:09.0/hwmon/hwmon2',
+            waitFile=waitFile)
+      ledSysfsDriver = LedSysfsDriver(sysfsPath='/sys/class/leds')
+      drivers = drivers or [fanSysfsDriver, ledSysfsDriver]
+      super(ScdFanComponent, self).__init__(drivers=drivers, **kwargs)
+
+   def createFan(self, fanId, driver='FanSysfsDriver', ledDriver='LedSysfsDriver',
+                 **kwargs):
+      logging.debug('creating scd fan %s', fanId)
       driver = self.drivers[driver]
       led = LedImpl(name='fan%s' % fanId, driver=self.drivers[ledDriver])
       return FanImpl(fanId=fanId, driver=driver, led=led, **kwargs)
