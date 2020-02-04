b0b182b84d768041ab2dc1602085e0c65d848cf5
Alex Michon <amichon@arista.com>
2020-02-03 18:26:09 -0800

    Cache prefdl in tmpfs
    Change-Id: I058c30d7476a4a9e6dfa3e89bc356248bca0b23e

--- arista/components/eeprom.py
+++ arista/components/eeprom.py
@@ -1,7 +1,11 @@
 
 from .common import I2cComponent
+from ..core.log import getLogger
 from ..core.prefdl import decodeBuffer
+from ..core.utils import JsonStoredData
 from ..drivers.eeprom import SeepromI2cDevDriver, EepromKernelDriver
+
+logging = getLogger(__name__)
 
 class I2cEeprom(I2cComponent):
    def __init__(self, addr, name=None, drivers=None, **kwargs):
@@ -21,12 +25,59 @@ class I2cSeeprom(I2cComponent):
    def read(self):
       return self.drivers['SeepromI2cDevDriver'].read()
 
-class PrefdlEeprom(I2cEeprom):
+class PrefdlBase(object):
    def prefdl(self):
+      cachedPrefdl = None
+      try:
+         cachedPrefdl = JsonStoredData(self.cacheFile())
+         if cachedPrefdl.exist():
+            return cachedPrefdl.read()
+      except:
+         logging.debug('Failed to read cached prefdl for device %s' %
+               self.prefdlAddr())
+
+      data = self.decode()
+
+      if cachedPrefdl:
+         try:
+            cachedPrefdl.write(data, mode='w')
+         except:
+            logging.debug('Failed to cache prefdl for device %s' % self.prefdlAddr())
+
+      return data
+
+   def clean(self):
+      cachedPrefdl = JsonStoredData(self.cacheFile())
+      cachedPrefdl.clear()
+
+   def cacheFile(self):
+      return 'prefdl_%s' % self.prefdlAddr()
+
+   def decode(self):
+      raise NotImplementedError
+
+   def prefdlAddr(self):
+      raise NotImplementedError
+
+class PrefdlEeprom(I2cEeprom, PrefdlBase):
+   def decode(self):
       return decodeBuffer(self.read()).data()
 
-class PrefdlSeeprom(I2cSeeprom):
-   def prefdl(self):
+   def prefdlAddr(self):
+      return str(self.addr)
+
+   def clean(self):
+      I2cEeprom.clean(self)
+      PrefdlBase.clean(self)
+
+class PrefdlSeeprom(I2cSeeprom, PrefdlBase):
+   def decode(self):
       data = self.read()
       return decodeBuffer(data[8:]).data()
 
+   def prefdlAddr(self):
+      return str(self.addr)
+
+   def clean(self):
+      I2cSeeprom.clean(self)
+      PrefdlBase.clean(self)
