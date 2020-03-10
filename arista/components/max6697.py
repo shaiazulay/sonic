f390e650dbb3b1ebe7137ae068aba7f5f8b923a7
Alex Michon <amichon@arista.com>
2020-03-10 14:23:25 -0700

    Add temp accessors to inventory
    Change-Id: I5f7d5f31a6f7cff0ff6ca74dc02d001e2a3308cb

--- arista/components/max6697.py
+++ arista/components/max6697.py
@@ -1,15 +1,22 @@
 
 from .common import I2cComponent
+from ..accessors.temp import TempImpl
 from ..core.component import Priority
 from ..drivers.max6697 import Max6697KernelDriver
+from ..drivers.sysfs import TempSysfsDriver
 
 class Max6697(I2cComponent):
    def __init__(self, addr, drivers=None,
-                priority=Priority.THERMAL, **kwargs):
-      drivers = drivers or [ Max6697KernelDriver(addr=addr) ]
+                priority=Priority.THERMAL, sensors=None, **kwargs):
+      drivers = drivers or [Max6697KernelDriver(addr=addr),
+                            TempSysfsDriver(addr=addr)]
       self.sensors = []
       super(Max6697, self).__init__(addr=addr, drivers=drivers, priority=priority,
                                     **kwargs)
+      if sensors:
+         self.addTempSensors(sensors)
 
    def addTempSensors(self, sensors):
       self.sensors.extend(sensors)
+      for sensor in sensors:
+         self.inventory.addTemp(TempImpl(sensor, self.drivers['TempSysfsDriver']))
--- arista/components/tmp468.py
+++ arista/components/tmp468.py
@@ -1,15 +1,22 @@
 
 from .common import I2cComponent
+from ..accessors.temp import TempImpl
 from ..core.component import Priority
+from ..drivers.sysfs import TempSysfsDriver
 from ..drivers.tmp468 import Tmp468KernelDriver
 
 class Tmp468(I2cComponent):
    def __init__(self, addr, drivers=None,
-                priority=Priority.THERMAL, **kwargs):
-      drivers = drivers or [ Tmp468KernelDriver(addr=addr) ]
+                priority=Priority.THERMAL, sensors=None, **kwargs):
+      drivers = drivers or [Tmp468KernelDriver(addr=addr),
+                            TempSysfsDriver(addr=addr)]
       self.sensors = []
       super(Tmp468, self).__init__(addr=addr, drivers=drivers, priority=priority,
                                    **kwargs)
+      if sensors:
+         self.addTempSensors(sensors)
 
    def addTempSensors(self, sensors):
       self.sensors.extend(sensors)
+      for sensor in sensors:
+         self.inventory.addTemp(TempImpl(sensor, self.drivers['TempSysfsDriver']))
