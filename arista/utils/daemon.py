fcbc6da4091edc0d4a850f7347a81a30ae465cc6
Samuel Angebault <staphylo@arista.com>
2020-03-17 11:40:40 -0700

    Check management mac address
    Change-Id: Ida9df30a7be9eaea40020fc08d52d7c5fb705d5e

--- arista/utils/daemon.py
+++ arista/utils/daemon.py
@@ -4,6 +4,33 @@ from __future__ import absolute_import, division, print_function
 import logging
 import time
 
+from ..core.utils import getCmdlineDict
+from ..core.platform import getSysEeprom
+
+def readMacAddress(intfName):
+   path = '/sys/class/net/%s/address' % intfName
+   with open(path) as f:
+      return f.read().rstrip()
+
+def checkMgmtMacAddress(intfName='eth0'):
+   currentMac = readMacAddress(intfName)
+   desiredMac = getSysEeprom().get('MAC', '00:00:00:00:00:00')
+   currentMacValue = int(currentMac.replace(':', ''), 16)
+   desiredMacValue = int(desiredMac.replace(':', ''), 16)
+   if currentMac == desiredMac:
+      logging.info('mac address of %s is properly set to %s', intfName,
+                   currentMac)
+   elif currentMacValue == desiredMacValue + 1:
+      logging.warning('mac address of %s is not set properly. is %s should be %s'
+                      ' (likely coming from EOS via fast-reboot)',
+                      intfName, currentMac, desiredMac)
+   else:
+      logging.error('mac address of %s is not set properly. is %s should be %s',
+                    intfName, currentMac, desiredMac)
+      for key, value in getCmdlineDict().items():
+         if key.startswith('hwaddr_'):
+            logging.info('cmdline: %s=%s', key, value)
+
 def runDaemon(platform, interval=60):
    hasSysCpld = False
    seuErrorDetected = False
@@ -17,6 +44,11 @@ def runDaemon(platform, interval=60):
          platform.syscpld.powerCycleOnSeu(False)
       else:
          logging.info('powercycle on SEU already disabled')
+
+   try:
+      checkMgmtMacAddress()
+   except:
+      logging.error('failed to check mgmt mac address')
 
    while True:
       if hasSysCpld and not seuErrorDetected and platform.syscpld.hasSeuError():
