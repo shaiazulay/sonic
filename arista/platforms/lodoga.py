9ea7795ac085452bd14e4146a446ef842152e7de
Zhi Yuan Carl Zhao <zyzhao@arista.com>
2020-05-22 11:59:18 -0700

    Add temp sensors for Lodoga
    Change-Id: I6b3c72f2bb8f4b1816956001146f975ec8565abd

--- arista/platforms/lodoga.py
+++ arista/platforms/lodoga.py
@@ -4,6 +4,7 @@ from ..core.types import PciAddr, I2cAddr, NamedGpio, ResetGpio
 from ..core.utils import incrange
 
 from ..components.asic.xgs.trident3 import Trident3
+from ..components.cpu.amd.k10temp import K10Temp
 from ..components.cpu.crow import CrowSysCpld, CrowFanCpldComponent
 from ..components.dpm import Ucd90120A, UcdGpi
 from ..components.max6658 import Max6658
@@ -11,6 +12,8 @@ from ..components.psu import PmbusPsu
 from ..components.scd import Scd
 
 from ..descs.fan import FanDesc
+from ..descs.psu import PsuDesc
+from ..descs.sensor import Position, SensorDesc
 
 @registerPlatform()
 class Lodoga(FixedSystem):
@@ -32,8 +35,18 @@ class Lodoga(FixedSystem):
 
       scd.createWatchdog()
 
+      self.newComponent(K10Temp, sensors=[
+         SensorDesc(diode=0, name='Cpu temp sensor',
+                    position=Position.OTHER, target=60, overheat=90, critical=95),
+      ])
+
       scd.newComponent(Max6658, scd.i2cAddr(0, 0x4c),
-                       waitFile='/sys/class/hwmon/hwmon2')
+                       waitFile='/sys/class/hwmon/hwmon2', sensors=[
+         SensorDesc(diode=0, name='Cpu board temp sensor',
+                    position=Position.OTHER, target=55, overheat=75, critical=80),
+         SensorDesc(diode=1, name='Back-panel temp sensor',
+                    position=Position.OUTLET, target=50, overheat=75, critical=85),
+      ])
       scd.newComponent(Ucd90120A, scd.i2cAddr(0, 0x4e, t=3))
 
       scd.newComponent(CrowFanCpldComponent, addr=scd.i2cAddr(0, 0x60),
@@ -42,16 +55,12 @@ class Lodoga(FixedSystem):
       ])
 
       scd.newComponent(Max6658, scd.i2cAddr(9, 0x4c),
-                       waitFile='/sys/class/hwmon/hwmon4')
-      scd.newComponent(PmbusPsu, scd.i2cAddr(11, 0x58, t=3, datr=2, datw=3))
-      scd.newComponent(PmbusPsu, scd.i2cAddr(12, 0x58, t=3, datr=2, datw=3))
-      scd.newComponent(Ucd90120A, scd.i2cAddr(13, 0x4e, t=3), causes={
-         'reboot': UcdGpi(1),
-         'watchdog': UcdGpi(2),
-         'overtemp': UcdGpi(4),
-         'powerloss': UcdGpi(5),
-         'systempowerloss': UcdGpi(6),
-      })
+                       waitFile='/sys/class/hwmon/hwmon4', sensors=[
+         SensorDesc(diode=0, name='Board temp sensor',
+                    position=Position.OTHER, target=65, overheat=75, critical=85),
+         SensorDesc(diode=1, name='Front-panel temp sensor',
+                    position=Position.INLET, target=50, overheat=60, critical=65),
+      ])
 
       scd.addSmbusMasterRange(0x8000, 6, 0x80)
 
@@ -62,6 +71,35 @@ class Lodoga(FixedSystem):
          (0x6080, 'psu2'),
          (0x6090, 'beacon'),
       ])
+
+      for psuId in incrange(1, 2):
+         scd.addPsu(PmbusPsu,
+                    addr=scd.i2cAddr(10 + psuId, 0x58, t=3, datr=2, datw=3),
+                    waitFile='/sys/class/hwmon/hwmon%d' % (4 + psuId), psus=[
+            PsuDesc(psuId=psuId, led=self.inventory.getLed('psu%s' % psuId),
+                    sensors=[
+               SensorDesc(diode=0,
+                          name='Power supply %d hotspot sensor' % psuId,
+                          position=Position.OTHER,
+                          target=80, overheat=95, critical=100),
+               SensorDesc(diode=1,
+                          name='Power supply %d inlet temp sensor' % psuId,
+                          position=Position.OTHER,
+                          target=55, overheat=70, critical=75),
+               SensorDesc(diode=2,
+                          name='Power supply %d secondary hotspot sensor' % psuId,
+                          position=Position.OTHER,
+                          target=80, overheat=108, critical=113),
+            ]),
+         ])
+
+      scd.newComponent(Ucd90120A, scd.i2cAddr(13, 0x4e, t=3), causes={
+         'reboot': UcdGpi(1),
+         'watchdog': UcdGpi(2),
+         'overtemp': UcdGpi(4),
+         'powerloss': UcdGpi(5),
+         'systempowerloss': UcdGpi(6),
+      })
 
       scd.addResets([
          ResetGpio(0x4000, 1, False, 'switch_chip_reset'),
@@ -79,8 +117,6 @@ class Lodoga(FixedSystem):
          NamedGpio(0x5000, 11, True, False, "psu1_ac_status"),
          NamedGpio(0x5000, 10, True, False, "psu2_ac_status"),
       ])
-      scd.createPsu(1, led=self.inventory.getLed('psu1'))
-      scd.createPsu(2, led=self.inventory.getLed('psu2'))
 
       addr = 0x6100
       for xcvrId in self.sfpRange:
@@ -123,4 +159,3 @@ class Lodoga(FixedSystem):
                      leds=self.inventory.getLedGroup(name))
          addr += 0x10
          bus += 1
-
