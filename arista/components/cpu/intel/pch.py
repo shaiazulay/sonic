69295e82dd44aabdc0035c910f9c35adf1c522c6
Boyang Yu <byu@arista.com>
2020-06-01 16:58:31 -0700

    Fix pch sysfs path for buster
    Change-Id: Ibb17039bdba1a6280cf7d239eca3c1b81eba80dd

--- arista/components/cpu/intel/pch.py
+++ arista/components/cpu/intel/pch.py
@@ -8,6 +8,6 @@ class Pch(PciComponent, TempComponentMixin):
    def __init__(self, drivers=None, priority=Priority.THERMAL,
                 waitFile='/sys/class/hwmon/hwmon0', **kwargs):
       drivers = drivers or [TempSysfsDriver(driverName='tempDriver',
-                            sysfsPath='/sys/devices/virtual/hwmon/hwmon0')]
+                            sysfsPath='/sys/devices/virtual/thermal/thermal_zone0/hwmon0')]
       super(Pch, self).__init__(drivers=drivers, priority=priority,
                                 waitFile=waitFile, **kwargs)
