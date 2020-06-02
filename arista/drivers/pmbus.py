2af74a0b19e6835c7d6a7c225f8c6cc108f99bb7
Zhi Yuan Carl Zhao <zyzhao@arista.com>
2020-06-02 14:42:58 -0700

    Change Pmbus PSU driver to only need one gpio input
    Not all PSUs seem to have all the gpios at non-zero value, like the
    Blackhawk PSUs only have curr1_input non-zero but not curr2_input, I
    think it's ok to treat that as OK status.
    
    Change-Id: I8973d3240015a5c1d29897d0f2cf97a63871e5e9

--- arista/drivers/pmbus.py
+++ arista/drivers/pmbus.py
@@ -32,20 +32,18 @@ class PmbusDriver(Driver):
    @simulateWith(getStatusSim_)
    def getPsuStatus(self, psu):
       # At least one sensor is expected to exist, otherwise treat it as a failure.
-      nonZero = False
       # Check input and output values of current and voltage are in the range.
       for sensor in self.sensors:
+         nonZero = False
          # The value must be non zero.
          value, exists = self.readSensor('%s_input' % sensor)
-         if not exists:
+         if not exists or not value:
             continue
-         elif not value:
-            return False
          nonZero = True
 
          # The value must be lower than its critical value.
          valueCrit, exists = self.readSensor('%s_crit' % sensor)
-         if exists and valueCrit and value > valueCrit:
+         if exists and valueCrit > 0 and value > valueCrit:
             return False
 
          # The value must be greater than its lowest allowed value.
@@ -53,4 +51,8 @@ class PmbusDriver(Driver):
          if exists and value < valueLCrit:
             return False
 
-      return nonZero
+         # Not all PSUs will have all the curr/in values, so we just need one
+         if nonZero:
+            return True
+
+      return False
